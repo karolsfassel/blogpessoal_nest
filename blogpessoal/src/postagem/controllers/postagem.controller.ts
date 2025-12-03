96b848ee8329c0ede23a4269bf4152904f020fca
Karol <karolfassel@gmail.com>
2025-12-03 18:04:12 -0300

    feat: adicionar CRUD com métodos findById e findByTitulo

--- blogpessoal/src/postagem/controllers/postagem.controller.ts
+++ blogpessoal/src/postagem/controllers/postagem.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { PostagemService} from '../services/postagem.service';
 import{Postagem} from '../entities/postagem.entity';
 
@@ -13,4 +13,40 @@ export class PostagemController{
  findAll(): Promise<Postagem[]>{
     return this.postagemService.findAll();
  }
+
+  @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem>{
+        return this.postagemService.findById(id);
+    }
+
+
+
+    @Get('/titulo/:titulo')
+    @HttpCode(HttpStatus.OK)
+    findByAllTitulo(@Param('titulo')titulo:string): Promise<Postagem[]>{
+      return this.postagemService.findAllByTitulo(titulo);
+    }
+
+
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() postagem: Postagem): Promise<Postagem>{
+      return this.postagemService.create(postagem);
+    }
+
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  Delete(@Param('id', ParseIntPipe)id: number){
+    return this.postagemService.delete(id);
+  }
+
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() postagem: Postagem): Promise <Postagem>{
+      return this.postagemService.update(postagem); 
+    }
 }
