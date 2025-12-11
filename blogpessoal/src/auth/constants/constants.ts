0ee63b3e01a8a7520e1c04a108d601d554bc95a2
Karol <karolfassel@gmail.com>
2025-12-11 17:37:32 -0300

    feat: Modulo auth

---
+++ blogpessoal/src/auth/constants/constants.ts
@@ -0,0 +1,6 @@
+
+
+
+export const jwtConstants ={
+    secret: '13cb79f534560fff3677830aab9d17d170e64ba8dc28848c8926a40ac0bcd6bf',
+};
---
+++ blogpessoal/src/auth/interfaces/usuario-sem-senha.interface.ts
@@ -0,0 +1,11 @@
+import { Postagem } from "../../postagem/entities/postagem.entity";
+
+
+
+export interface UsuarioSemSenha{
+    id: number;
+    nome: string;
+    usuario: string;
+    foto: string;
+    postagem: Postagem[];
+}
